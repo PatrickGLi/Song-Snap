adfe72326c23e18b32a6a954410c63b76d6255f9
Patrick Li <patrickli727@gmail.com>
2016-03-08 18:35:01 -0800

    Fix.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,31 +2,31 @@ class StaticPagesController < ApplicationController
   def root
 
   end
-
-  def signin
-    client = Soundcloud.new(:client_id => ENV["SOUNDCLOUD_CLIENT_ID"],
-                        :client_secret => ENV["SOUNDCLOUD_CLIENT_SECRET"],
-                        :redirect_uri => 'https://song-snap.herokuapp.com/request_access_token')
-
-    # redirect user to authorize URL
-    redirect_to client.authorize_url()
-  end
-
-  def request_access_token
-    client = Soundcloud.new(:client_id => ENV["SOUNDCLOUD_CLIENT_ID"],
-                            :client_secret => ENV["SOUNDCLOUD_CLIENT_SECRET"],
-                            :redirect_uri => 'http://example.com/callback')
-
-    debugger
-
-    code = params[:code]
-    access_token = client.exchange_token(:code => code)
-  end
 end
+#   def signin
+#     client = Soundcloud.new(:client_id => ENV["SOUNDCLOUD_CLIENT_ID"],
+#                         :client_secret => ENV["SOUNDCLOUD_CLIENT_SECRET"],
+#                         :redirect_uri => '#{request_access_token_url}')
+#
+#     # redirect user to authorize URL
+#     redirect_to client.authorize_url()
+#   end
+#
+#   def request_access_token
+#     client = Soundcloud.new(:client_id => ENV["SOUNDCLOUD_CLIENT_ID"],
+#                             :client_secret => ENV["SOUNDCLOUD_CLIENT_SECRET"],
+#                             :redirect_uri => 'http://example.com/callback')
+#
+#     debugger
+#
+#     code = params[:code]
+#     access_token = client.exchange_token(:code => code)
+#   end
+# end
 
 
-
-# 
+#
+# #
 # The redirect URI typically points to a server-side script that requests the access token through a POST to the authorization server.
 #
 #
