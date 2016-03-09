aa7eeaacbd701e67f5451d2f852ad88d025482e8
Patrick Li <patrickli727@gmail.com>
2016-03-08 19:53:27 -0800

    Changed redirect uri.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,7 @@ class StaticPagesController < ApplicationController
   def signin
     client = Soundcloud.new(:client_id => ENV["SOUNDCLOUD_CLIENT_ID"],
                         :client_secret => ENV["SOUNDCLOUD_CLIENT_SECRET"],
-                        :redirect_uri => 'https://example.com')
+                        :redirect_uri => 'https://song-snap.herokuapp.com/callback')
 
     # redirect user to authorize URL
     redirect_to client.authorize_url()
